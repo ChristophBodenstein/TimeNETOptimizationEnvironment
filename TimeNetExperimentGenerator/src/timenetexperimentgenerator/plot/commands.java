95b077e3971acb82e0611428792f2dc6c1e2fa51
sniebler <sniebler>
2014-07-10 12:17:23 +0000

    r-plugin template added

---
+++ TimeNetExperimentGenerator/src/timenetexperimentgenerator/plot/commands.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package timenetexperimentgenerator.plot;
+
+/**
+ *
+ * @author Simon
+ */
+public class commands 
+{
+    public String _comPlot;
+    public String _comAdd;
+    //etc.
+    
+}
