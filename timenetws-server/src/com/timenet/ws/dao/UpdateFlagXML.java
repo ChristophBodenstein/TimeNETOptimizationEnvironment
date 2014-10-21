91985f1e66dd1fa461a51d768ade00e8582a7b9b
veeranna <veeranna>
2014-10-21 22:12:54 +0000

    Class updated for 1. Comment for logic and methods 2. Proper Logging Comment 3. Null Pointer Exception Handled

--- timenetws-server/src/com/timenet/ws/dao/UpdateFlagXML.java
+++ timenetws-server/src/com/timenet/ws/dao/UpdateFlagXML.java
@@ -1,6 +1,10 @@
 package com.timenet.ws.dao;
-
+/**
+ * auto GeneratedValue
+ * @author Veeranna
+ */
 import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
@@ -9,6 +13,7 @@ import javax.ws.rs.core.Response;
 import com.timenet.ws.models.simulationLogList;
 import com.timenet.ws.models.simulationXmlList;
 import com.timenet.ws.util.HibernateTemplate;
+import com.timenet.ws.util.LogFile;
 
 
 @Path("/api")
@@ -23,25 +28,34 @@ public class UpdateFlagXML {
 		
 		simulationXmlList stdXML =null;
 		List stdLog =null;
-		System.out.println("Flag Update XML ============="+ filename+"----"+simid);
+		LogFile.info("Flag Update XML ============="+ filename+"----"+simid);
+		
+		if(filename != null && simid != null)
+		{
 		String HQLQuerryLog=" from "+ simulationLogList.class.getName()+" where log_file_name like ? and simulation_id=?";
 	    stdLog= HibernateTemplate.findList(HQLQuerryLog, 0, 1, filename+"%", simid);
 		    
 		if (stdLog.size()==0) {
-			String HQLQuerryXML=" from "+ simulationXmlList.class.getName()+" where simulation_file_name= ? and simulation_id= ?";
-		    stdXML=(simulationXmlList) HibernateTemplate.findList(HQLQuerryXML, 0, 1, filename+".xml", simid).get(0);
 			
-		stdXML.setDistribute_flag("ND");
-		HibernateTemplate.update(stdXML);
-		System.out.println("--------Simulation XML file Flag updated D to ND---------"+stdXML.getXml_file_name());
-		
-		Response.status(200);
-			 return Response;
-		
+	try
+		{
+			String HQLQuerryXML=" from "+ simulationXmlList.class.getName()+" where simulation_file_name= ? and simulation_id= ?";
+			stdXML=(simulationXmlList) HibernateTemplate.findList(HQLQuerryXML, 0, 1, filename+".xml", simid).get(0);
+			stdXML.setDistribute_flag("ND");
+			HibernateTemplate.update(stdXML);
+			LogFile.info("--------Simulation XML file Flag updated D to ND---------"+stdXML.getXml_file_name());
+			}
+		catch(Exception e)
+		{
+			    LogFile.debug("Simulation XML file exception occured");
+	     }  
+			Response.status(200);
+			}
+		else{
+			// http status 412 Precondition Failed
+			Response.status(412);
 		}
-		else {
-			Response.status(401);
+			}
 			return Response;
-		 }
-	}	
-}
+		}	
+	}
