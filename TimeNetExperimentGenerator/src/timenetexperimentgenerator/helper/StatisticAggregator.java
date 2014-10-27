b84f0b72d5f682eab482229b690f75e7d771a780
Christoph Bodenstein <Christoph.Bodenstein@tu-ilmenau.de>
2014-10-27 14:16:34 +0000

    completed statistics output for optimizations

--- TimeNetExperimentGenerator/src/timenetexperimentgenerator/helper/StatisticAggregator.java
+++ TimeNetExperimentGenerator/src/timenetexperimentgenerator/helper/StatisticAggregator.java
@@ -11,6 +11,7 @@
 package timenetexperimentgenerator.helper;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import timenetexperimentgenerator.Parser;
 import timenetexperimentgenerator.datamodel.SimulationType;
 import timenetexperimentgenerator.support;
@@ -78,6 +79,8 @@ private static ArrayList<Statistic> listOfStatistics=new ArrayList<Statistic>();
         for(int i=0;i<listOfStatistics.size();i++){
         listOfStatistics.get(i).printStatisticToLog();
         }
+        //Print all Optimization Statistics if available
+        printOptiStatistics();
     }
     
     /**
@@ -108,4 +111,88 @@ private static ArrayList<Statistic> listOfStatistics=new ArrayList<Statistic>();
         return listOfStatistics;
     }
     
+    /**
+     * Prints all Optimization statistics and a summary of the last optimizations
+     */
+    public static void printOptiStatistics(){
+    int numberOfAllOptimizationRuns=0;
+    double averageDistanceToOptimumAbsolute=0;
+    double averageDistanceToOptimumRelative=0;
+    double averageDistanceToOptimumInDesignSpace=0;
+    int numberOfFoundOptimaWithinRangeClass[]=new int[support.DEFAULT_NUMBER_OF_OPTI_PROB_CLASSES];
+    int numberOfFoundOptimaWithinRangeClassDS[]=new int[support.DEFAULT_NUMBER_OF_OPTI_PROB_CLASSES];
+    
+    double targetDistanceToCalculatedOptimum=1;//in %
+    double targetDistanceToCalculatedOptimumDS=1;//in %
+    double probabilityThatFoundOptimumIsInDistanceOfXAroundCalculatedOptimum=0;//calc this for different X
+    double probabilityThatFoundOptimumIsInDistanceOfXAroundCalculatedOptimumDS=0;//calc this for different X
+    
+    //init RangeClasses
+        for(int i=0;i<support.DEFAULT_NUMBER_OF_OPTI_PROB_CLASSES;i++){
+        numberOfFoundOptimaWithinRangeClass[i]=0;
+        numberOfFoundOptimaWithinRangeClassDS[i]=0;
+        }
+    
+    ArrayList<Statistic> optiSimulationList=new ArrayList<Statistic>();
+    //Fill new ArrayList with Statistic that are Optimiation
+    for (Statistic s : listOfStatistics) {
+        if(s.isOptimization()){
+            optiSimulationList.add(s);
+            averageDistanceToOptimumAbsolute+=s.getDistanceToTargetValue();
+            averageDistanceToOptimumRelative+=s.getDistanceRelative();
+            averageDistanceToOptimumInDesignSpace+=s.getDistanceToTargetDS();
+            //Calculate RangeClasses
+            for(int i=1;i<=support.DEFAULT_NUMBER_OF_OPTI_PROB_CLASSES;i++){
+                if(s.getDistanceRelative()<=i*100/support.DEFAULT_NUMBER_OF_OPTI_PROB_CLASSES){
+                numberOfFoundOptimaWithinRangeClass[i-1]++;
+                }
+                if(s.getDistanceToTargetDS()<=i*100/support.DEFAULT_NUMBER_OF_OPTI_PROB_CLASSES){
+                numberOfFoundOptimaWithinRangeClassDS[i-1]++;
+                }
+            }
+            
+        }
+    }
+    numberOfAllOptimizationRuns=optiSimulationList.size();
+    averageDistanceToOptimumAbsolute=averageDistanceToOptimumAbsolute/numberOfAllOptimizationRuns;
+    averageDistanceToOptimumRelative=averageDistanceToOptimumRelative/numberOfAllOptimizationRuns;
+    averageDistanceToOptimumInDesignSpace=averageDistanceToOptimumInDesignSpace/numberOfAllOptimizationRuns;
+    
+    
+        support.log("++++ Start of Optimization Statistics ++++");
+        support.log("Number of Optimization runs: "+numberOfAllOptimizationRuns);
+        support.log("Average distance to calculated optimum (absolute): "+averageDistanceToOptimumAbsolute);
+        support.log("Average distance to calculated optimum (relative): "+averageDistanceToOptimumRelative +" %");
+        support.log("Average distance to calculated optimum in Design Space " +averageDistanceToOptimumInDesignSpace+" %");
+        String tmpOutHead=      "Radius:    ";
+        String tmpOutValue=     "Values:    ";
+        String tmpOutDSValue=   "DS-Values: ";
+            for(int i=1;i<=support.DEFAULT_NUMBER_OF_OPTI_PROB_CLASSES;i++){
+            tmpOutHead+="| "+support.padLeft( String.valueOf(i*100/support.DEFAULT_NUMBER_OF_OPTI_PROB_CLASSES),8) ;
+            
+            //support.log("# of Optima within Radius of " +(i*100/support.DEFAULT_NUMBER_OF_OPTI_PROB_CLASSES)+" around Calculated Optimum: "+ numberOfFoundOptimaWithinRangeClass[i-1] +" --  "+(numberOfFoundOptimaWithinRangeClass[i-1]*100/numberOfAllOptimizationRuns)+"%");    
+            tmpOutValue+="| "+support.padLeft(numberOfFoundOptimaWithinRangeClass[i-1] +"--"+(numberOfFoundOptimaWithinRangeClass[i-1]*100/numberOfAllOptimizationRuns)+"%",8); 
+            //support.log("# of Optima within DS-Radius of " +(i*100/support.DEFAULT_NUMBER_OF_OPTI_PROB_CLASSES)+" around Calculated Optimum: "+ numberOfFoundOptimaWithinRangeClassDS[i-1] +" --  "+(numberOfFoundOptimaWithinRangeClassDS[i-1]*100/numberOfAllOptimizationRuns)+"%");        
+            tmpOutDSValue+="| "+support.padLeft(numberOfFoundOptimaWithinRangeClassDS[i-1] +"--"+(numberOfFoundOptimaWithinRangeClassDS[i-1]*100/numberOfAllOptimizationRuns)+"%",8); 
+            }
+        support.log(tmpOutHead);
+        support.log(tmpOutValue);
+        support.log(tmpOutDSValue);
+        support.log("++++ End of Optimization Statistics ++++");
+    }
+    
+    /**
+     * Removes all optimization statistics from List of statistics
+     */
+    public static void removeOldOptimizationsFromList(){
+    Iterator<Statistic> it=listOfStatistics.iterator();
+    
+        while(it.hasNext()){
+        Statistic s=it.next();
+            if(s.isOptimization()){
+            it.remove();
+            }
+        }
+    
+    }
 }
