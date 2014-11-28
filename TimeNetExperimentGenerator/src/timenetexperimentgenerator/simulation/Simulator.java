9b49b0bff6fc0ea5a1068b755da4ff1e15ae681e
Christoph Bodenstein <Christoph.Bodenstein@tu-ilmenau.de>
2014-11-28 11:56:03 +0000

    better code formating, added comments. removed unneccesary variables

--- TimeNetExperimentGenerator/src/timenetexperimentgenerator/simulation/Simulator.java
+++ TimeNetExperimentGenerator/src/timenetexperimentgenerator/simulation/Simulator.java
@@ -4,7 +4,6 @@
  * Christoph Bodenstein
  * TU-Ilmenau, FG SSE
  */
-
 package timenetexperimentgenerator.simulation;
 
 import java.util.ArrayList;
@@ -17,9 +16,46 @@ import timenetexperimentgenerator.datamodel.SimulationType;
  * @author Christoph Bodenstein
  */
 public interface Simulator {
-public void initSimulator(ArrayList< ArrayList<parameter> > listOfParameterSetsTMP, int simulationCounterTMP, boolean log);
-public int getStatus();
-public int getSimulationCounter();
-public ArrayList<SimulationType> getListOfCompletedSimulationParsers();
-public SimulationType getCalculatedOptimum(MeasureType targetMeasure);
+
+    /**
+     * Iits the simulator with all necessary varibales and starts the Thread So
+     * the simulator-thread don`t need to be started external!
+     *
+     * @param listOfParameterSetsTMP ArrayList of parametersets (ArrayList) to
+     * be simulated
+     * @param simulationCounterTMP actual simualtion counter, to be increased by
+     * this simulator (deprecated)
+     * @param log boolean value whether to write results to a separate log file
+     * or not
+     */
+    public void initSimulator(ArrayList< ArrayList<parameter>> listOfParameterSetsTMP, int simulationCounterTMP, boolean log);
+
+    /**
+     *
+     * @return status of simulator (0..100) in % if 100 then simulation has
+     * ended
+     */
+    public int getStatus();
+
+    /**
+     *
+     * @return actual simulation counter (deprecated)
+     */
+    public int getSimulationCounter();
+
+    /**
+     *
+     * @return List of completed simulations as parser-objects to be used for
+     * optimization or export
+     */
+    public ArrayList<SimulationType> getListOfCompletedSimulationParsers();
+
+    /**
+     *
+     * @param targetMeasure Measure to be optimized (Measure incl. target value
+     * etc.)
+     * @return Simulation which is the best for given Measure and target, if it
+     * can be calculated (only useful for benchmark, Cache-* simulations)
+     */
+    public SimulationType getCalculatedOptimum(MeasureType targetMeasure);
 }
