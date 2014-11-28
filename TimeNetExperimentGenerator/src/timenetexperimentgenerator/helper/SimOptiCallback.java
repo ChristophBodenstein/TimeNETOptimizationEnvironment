6c2b26542b087b95a41cc385a80ae1a54378ca21
Christoph Bodenstein <Christoph.Bodenstein@tu-ilmenau.de>
2014-11-28 12:13:41 +0000

    better code formating, added comments.

--- TimeNetExperimentGenerator/src/timenetexperimentgenerator/helper/SimOptiCallback.java
+++ TimeNetExperimentGenerator/src/timenetexperimentgenerator/helper/SimOptiCallback.java
@@ -16,6 +16,19 @@ import timenetexperimentgenerator.typedef;
  */
 public interface SimOptiCallback {
 
+    /**
+     * Is called after simulation has finished successful
+     *
+     * @param message to be displayed or printed
+     * @param feedback Type of feedback (successful, canceled, etc.)
+     */
     public void operationSucessfull(String message, typedef.typeOfProcessFeedback feedback);
+
+    /**
+     * Is called after simulation has been canceled
+     *
+     * @param message to be displayed or printed
+     * @param feedback Type of feedback, mostly "canceled"
+     */
     public void operationCanceled(String message, typedef.typeOfProcessFeedback feedback);
 }
