e13e1d9de98e33d22eaade366c4c029dff0e759d
Christoph Bodenstein <Christoph.Bodenstein@tu-ilmenau.de>
2014-11-28 12:00:15 +0000

    better code formating, added comments

--- TimeNetExperimentGenerator/src/timenetexperimentgenerator/helper/nativeProcess.java
+++ TimeNetExperimentGenerator/src/timenetexperimentgenerator/helper/nativeProcess.java
@@ -9,7 +9,6 @@
  * Christoph Bodenstein
  * TU-Ilmenau, FG SSE
  */
-
 package timenetexperimentgenerator.helper;
 
 import javax.swing.JLabel;
@@ -19,51 +18,58 @@ import timenetexperimentgenerator.support;
  *
  * @author Christoph Bodenstein
  */
-public class nativeProcess extends Thread{
-private ProcessBuilder myProcessBuilder=null;
-JLabel statusLabel=null;
-private boolean running=true;
-private nativeProcessCallbacks pfc=null;
+public class nativeProcess extends Thread {
+
+    private ProcessBuilder myProcessBuilder = null;
+    JLabel statusLabel = null;
+    private boolean running = true;
+    private nativeProcessCallbacks pfc = null;
 
     /**
      * Contructor
+     *
      * @param p ProcessBuilder Object, this process will be started
-     * @param processCallbackHandler Callback handler (must implement Callback-methods)
-     * @param l JLabel to display a spinning char
+     * @param processCallbackHandler Callback handler (must implement
+     * Callback-methods)
      */
     public nativeProcess(ProcessBuilder p, nativeProcessCallbacks processCallbackHandler) {
-    this.myProcessBuilder=p;
-    this.pfc=processCallbackHandler;
-    new Thread(this).start();
+        this.myProcessBuilder = p;
+        this.pfc = processCallbackHandler;
+        new Thread(this).start();
     }
 
+    /**
+     * Main Method for thread. Will start the native process and wait for its
+     * end
+     *
+     */
     @Override
-    public void run(){
-    realNativeProcess myNativeProcess=new realNativeProcess(this);
-    myNativeProcess.start();
-    
-        while(isRunning()){
+    public void run() {
+        realNativeProcess myNativeProcess = new realNativeProcess(this);
+        myNativeProcess.start();
+
+        while (isRunning()) {
             try {
                 Thread.sleep(100);
                 support.spinInLabel();
-                if(support.isCancelEverything()){
-                //Kill native Thread and set Running to false
-                support.log("Try to kill the started process.");
-                myNativeProcess.killProcess();
-                this.setRunning(false);
-                this.pfc.errorOccured("Plot canceled.");
-                return;
+                if (support.isCancelEverything()) {
+                    //Kill native Thread and set Running to false
+                    support.log("Try to kill the started process.");
+                    myNativeProcess.killProcess();
+                    this.setRunning(false);
+                    this.pfc.errorOccured("Plot canceled.");
+                    return;
                 }
 
             } catch (InterruptedException ex) {
-                support.log("Error while waiting for real native Process: "+myProcessBuilder.toString());
+                support.log("Error while waiting for real native Process: " + myProcessBuilder.toString());
             }
         }
 
-        if(this.pfc!=null){
-        pfc.processEnded();
-        }else{
-        support.log("No PlotFramController given, will not show image.");
+        if (this.pfc != null) {
+            pfc.processEnded();
+        } else {
+            support.log("No PlotFramController given, will not show image.");
         }
 
     }
@@ -97,38 +103,39 @@ private nativeProcessCallbacks pfc=null;
     }
 }
 
-class realNativeProcess extends Thread{
-nativeProcess myNativeProcess=null;
-Process p=null;
+class realNativeProcess extends Thread {
+
+    nativeProcess myNativeProcess = null;
+    Process p = null;
 
     public realNativeProcess(nativeProcess p) {
-    myNativeProcess=p;
+        myNativeProcess = p;
     }
 
     @Override
-    public void run(){
-        if(myNativeProcess!=null){
+    public void run() {
+        if (myNativeProcess != null) {
             try {
                 p = myNativeProcess.getMyProcessBuilder().start();
                 p.waitFor();
                 myNativeProcess.setRunning(false);
             } catch (Exception ex) {
-                support.log("Error while waiting for nativeProcess:"+myNativeProcess.toString());
+                support.log("Error while waiting for nativeProcess:" + myNativeProcess.toString());
             }
 
-        }else{
-        support.log("No Process given, could not start anything.");
+        } else {
+            support.log("No Process given, could not start anything.");
         }
     }
-    
+
     /**
-     * Kills the running process.
-     * Remember, the started simulation-executable might still be running!
+     * Kills the running process. Remember, the started simulation-executable
+     * might still be running!
      */
-    public void killProcess(){
-        if(p!=null){
-        p.destroyForcibly();
-        support.log("Process will be killed. Check for running simulation binaries and kill them manually!!!");
+    public void killProcess() {
+        if (p != null) {
+            p.destroyForcibly();
+            support.log("Process will be killed. Check for running simulation binaries and kill them manually!!!");
         }
     }
 }
