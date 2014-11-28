430f39cc70ff132c6c958d216970a6243b88c824
Christoph Bodenstein <Christoph.Bodenstein@tu-ilmenau.de>
2014-11-28 13:17:49 +0000

    added license information

--- TimeNetExperimentGenerator/src/timenetexperimentgenerator/LogFrame.java
+++ TimeNetExperimentGenerator/src/timenetexperimentgenerator/LogFrame.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Frame to show logging information during runtime
+ * Christoph Bodenstein
+ * TU-Ilmenau, FG SSE
  */
 package timenetexperimentgenerator;
 
