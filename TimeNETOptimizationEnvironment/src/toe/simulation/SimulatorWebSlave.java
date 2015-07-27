6a10c810e2206f2d32552ec2c08a0739ce32504b
Christoph Bodenstein <Christoph.Bodenstein@tu-ilmenau.de>
2015-07-27 22:20:38 +0200

    Removed unused imports

--- TimeNETOptimizationEnvironment/src/toe/simulation/SimulatorWebSlave.java
+++ TimeNETOptimizationEnvironment/src/toe/simulation/SimulatorWebSlave.java
@@ -15,8 +15,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.http.HttpResponse;
