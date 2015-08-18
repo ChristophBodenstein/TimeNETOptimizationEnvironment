176ce0b6d00ef673ea7354aa61b32edf21ecc89c
ChristophBodenstein <Christoph.Bodenstein@tu-ilmenau.de>
2015-08-18 16:28:35 +0200

    version number

--- TimeNETOptimizationEnvironment/src/toe/support.java
+++ TimeNETOptimizationEnvironment/src/toe/support.java
@@ -40,7 +40,7 @@ import toe.typedef.typeOfProcessFeedback;
 public class support {
 
 //This Version of TimeNetExperimentGenerator
-    public static final String VERSION = "2015-08-03";
+    public static final String VERSION = "2015-08-18";
 
 //Define some program-wide default values
     public static final double DEFAULT_STEPPING = 1.0;
